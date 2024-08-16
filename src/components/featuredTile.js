4aaa47545dd2e3492f614901a67ed88a9add53d0
PhilLanzetta <71086048+PhilLanzetta@users.noreply.github.com>
2024-08-16 09:34:53 -0400

    featured tile

--- src/components/featuredTile.js
+++ src/components/featuredTile.js
@@ -1,19 +1,24 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import { GatsbyImage } from 'gatsby-plugin-image'
+import { Fade } from 'react-awesome-reveal'
 
 const FeaturedTile = ({ project }) => {
   const { slug, title, subtitle } = project.caseStudy
   return (
-    <Link to={`/${slug}`} className='featured-tile' >
-      <GatsbyImage
-        image={project.featuredImage.gatsbyImageData}
-        alt={project.featuredImage.description}
-        className='featured-image'
-      ></GatsbyImage>
-      <h3 className='featured-tile-title'>{title}</h3>
-      <h4 className='featured-tile-subtitle'>{subtitle}</h4>
-    </Link>
+    <Fade triggerOnce>
+      <Link to={`/${slug}`} className='featured-tile'>
+        <GatsbyImage
+          image={project.featuredImage.gatsbyImageData}
+          alt={project.featuredImage.description}
+          className='featured-image'
+        ></GatsbyImage>
+        <div className='featured-text-container'>
+          <h3 className='featured-tile-title'>{title}</h3>
+          <h4 className='featured-tile-subtitle'>{subtitle}</h4>
+        </div>
+      </Link>
+    </Fade>
   )
 }
 
