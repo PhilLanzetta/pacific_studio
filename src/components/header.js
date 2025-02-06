1212f45c8fd756ef20b601f8a525bd7adc628d77
PhilLanzetta <71086048+PhilLanzetta@users.noreply.github.com>
2025-02-06 13:28:02 -0500

    header

--- src/components/header.js
+++ src/components/header.js
@@ -4,20 +4,17 @@ import Logo from '../images/logo.svg'
 import HideOnScroll from './hideOnScroll'
 
 const Header = ({ location }) => {
-  console.log(location)
   const info = location?.pathname === '/info/'
   return (
     <header>
       <div>
         {info ? <Link to='/'>Close</Link> : <Link to='/info'>Information</Link>}
       </div>
-      <div className='header-logo'>
-        <HideOnScroll>
-          <Link to='/'>
-            <img src={Logo} alt='Pacific'></img>
-          </Link>
-        </HideOnScroll>
-      </div>
+      <HideOnScroll>
+        <Link to='/'>
+          <img src={Logo} alt='Pacific'></img>
+        </Link>
+      </HideOnScroll>
       <div></div>
     </header>
   )
